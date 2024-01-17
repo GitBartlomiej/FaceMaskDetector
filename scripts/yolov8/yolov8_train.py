dc4d9a0ce4a70b0c06d0cad9e84308eabc1c0986
bartlomiej <bartlomiej.piotrowski@unitedrobots.co>
2024-01-17 23:53:21 +0100

    Update the number of training epochs in yolov8_train.py to 35.

--- scripts/yolov8/yolov8_train.py
+++ scripts/yolov8/yolov8_train.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
 model = YOLO('yolov8n.pt') # załaduj wstępnie wytrenowany model
-model.train(data='/home/bartlomiej/Studia/Sem4/Przetwarzanie_Obrazów/Datasets/Mask_Classify.v7i.yolov8/data.yaml', epochs=2, imgsz=832, device=0)
+model.train(data='/home/bartlomiej/Studia/Sem4/Przetwarzanie_Obrazów/Datasets/Mask_Classify.v7i.yolov8/data.yaml', epochs=35, imgsz=832, device=0)
