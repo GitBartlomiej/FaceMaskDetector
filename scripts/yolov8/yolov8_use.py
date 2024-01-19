3990d4077bdb9faa050409669436eed0f98cc9d9
bartlomiej <bartlomiej.piotrowski@unitedrobots.co>
2024-01-20 00:17:10 +0100

    Add code to process and save images with detections from the specified directory.

--- scripts/yolov8/yolov8_use.py
+++ scripts/yolov8/yolov8_use.py
@@ -1,25 +1,32 @@
 import cv2
 from ultralytics import YOLO
+import os
 
-# Load your YOLO model with custom or pre-trained weights
+# Load the YOLO model
 model = YOLO('./runs/detect/train/weights/best.pt')
 
-# Run predictions on an image
-image_path = '/home/bartlomiej/Studia/Sem4/Przetwarzanie_Obrazów/face-masks/scripts/dataset/test/with_mask/black5.jpg'
-image = cv2.imread(image_path)
-results = model.predict(image)
+# Directory where the images are located
+source_dir = '/home/bartlomiej/Studia/Sem4/Przetwarzanie_Obrazów/face-masks/scripts/dataset/test/with_mask/'
 
-# Draw bounding boxes and labels on the image
-for box in results[0].boxes:
-    # Extract the bounding box coordinates
-    x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
-    # Determine the class label
-    label = results[0].names[box.cls[0].item()]
-    # Draw the bounding box rectangle and label on the image
-    image = cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
-    image = cv2.putText(image, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+# Directory where the images with detections will be saved
+output_dir = '/home/bartlomiej/Studia/Sem4/Przetwarzanie_Obrazów/face-masks/scripts/dataset/test/with_mask_detected/'
+os.makedirs(output_dir, exist_ok=True)
 
-# Display the image
-cv2.imshow('YOLOv8 Detection', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Process all images in the directory
+for image_name in os.listdir(source_dir):
+    if image_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+        image_path = os.path.join(source_dir, image_name)
+        image = cv2.imread(image_path)
+        results = model.predict(image)
+
+        # Draw bounding boxes and labels on the image
+        for box in results[0].boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+            label = results[0].names[box.cls[0].item()]
+            image = cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            image = cv2.putText(image, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+        # Save the image with detections
+        cv2.imwrite(os.path.join(output_dir, image_name), image)
+
+print('All images have been processed and saved with detections.')
