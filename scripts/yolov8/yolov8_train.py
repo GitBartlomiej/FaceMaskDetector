eb79008f7f7dc3d6b8169ea5db59406c57299058
bartlomiej <bartlomiej.piotrowski@unitedrobots.co>
2024-01-20 23:09:05 +0100

    Update yolov8_train.py script to train the model for 150 epochs.

--- scripts/yolov8/yolov8_train.py
+++ scripts/yolov8/yolov8_train.py
@@ -4,4 +4,4 @@ model = YOLO('yolov8n.pt')
 # model.train(data='/home/bartlomiej/Studia/Sem4/Przetwarzanie_Obrazów/Datasets/Mask_Classify.v7i.yolov8/data.yaml', epochs=100, imgsz=832, device=0)
 
 # Second model training
-model.train(data='/home/bartlomiej/Studia/Sem4/Przetwarzanie_Obrazów/face-masks/scripts/yolov8/dataset/data.yaml', epochs=100, imgsz=832, device=0)
+model.train(data='/home/bartlomiej/Studia/Sem4/Przetwarzanie_Obrazów/face-masks/scripts/yolov8/dataset/data.yaml', epochs=150, imgsz=832, device=0)
